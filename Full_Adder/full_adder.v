415c478b1c6af1cec01283ac8b5604b68d4153fa
kamatampadmasree56-ece <kamatampadmasree56@gmail.com>
2026-06-23 09:21:04 +0530

    Create full_adder.v
    A collection of VLSI design and Verilog HDL projects including logic gates, adders, flip-flops, CMOS circuits, simulations, and digital design experiments developed during my ECE studies.

---
+++ Full_Adder/full_adder.v
@@ -0,0 +1,12 @@
+module full_adder(
+    input a,
+    input b,
+    input Cin,
+    output sum,
+    output Cout
+);
+
+assign sum = a ^ b ^ Cin;
+assign Cout = (a & b) | (Cin & (a ^ b));
+
+endmodule
