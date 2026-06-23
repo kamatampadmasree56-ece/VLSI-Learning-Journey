c9ab7de693a683654e6cf28bc2d079076c168e8a
kamatampadmasree56-ece <kamatampadmasree56@gmail.com>
2026-06-23 09:50:23 +0530

    Create or_gate_tb.v
    Created testbench for OR Gate and verified all input combinations

---
+++ Full_Adder/Logic_Gates/or_gate_tb.v
@@ -0,0 +1,22 @@
+module or_gate_tb;
+
+reg a;
+reg b;
+wire y;
+
+// Instantiate the OR gate
+or_gate uut (
+    .a(a),
+    .b(b),
+    .y(y)
+);
+
+initial begin
+    a = 0; b = 0;
+    #10 a = 0; b = 1;
+    #10 a = 1; b = 0;
+    #10 a = 1; b = 1;
+    #10 $finish;
+end
+
+endmodule
